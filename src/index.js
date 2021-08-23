73af79ec5ccebffc37078102941023090cee3ccd
Artsiom Burdukou <artsiomburdukou@gmail.com>
2021-08-23 23:37:21 +0300

    feat: implement decode method in index.js

--- src/index.js
+++ src/index.js
@@ -39,6 +39,45 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    // let startIndex;
+    // let endIndex;
+    // let character = expr.slice(startIndex, endIndex);
+    let output = '';
+    const dot = '.';
+    const dash = '-';
+    const zero = '00';
+    const ten = '10';
+    const eleven = '11';
+    let startIndex, endIndex;
+
+    for (let i = 0; i < expr.length; i = i + 10) {
+        startIndex = i;
+        endIndex = startIndex + 10;
+        let character = expr.slice(startIndex, endIndex);
+        if (character[0] == "*") {
+            output += ' ';
+        }
+        else {
+            let morseString = ''
+            for (let i = 0; i < 10; i = i + 2) {
+                let firstTwoChars = character.slice(i, i + 2);
+                if (firstTwoChars == zero) {
+                    continue;
+                }
+                else if (firstTwoChars == ten) {
+                    morseString += dot;
+                }
+                else {
+                    morseString += dash;
+                }
+            }
+            output += MORSE_TABLE[morseString];
+            morseString = '';
+        }
+    }
+    
+    return output;
+
 }
 
 module.exports = {
